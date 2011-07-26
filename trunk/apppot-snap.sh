43ecbc365b52f7507074dd23429029bee99a9cc2
riccardo.murri <riccardo.murri@ebd855c4-8a32-5fcc-910f-1c7834090485>
2011-07-26 14:44:00 +0000

    Change extension of the default snapshot file to `.tar.gz`.
    Sanity checks and better reporting in `apppot-snap.sh`.

--- trunk/apppot-snap.sh
+++ trunk/apppot-snap.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 #
 PROG="$(basename $0)"
+VERSION=0.2
 
 usage () {
 cat <<EOF
@@ -40,7 +41,7 @@ EOF
 
 ## defaults
 
-base_snap_file='/var/lib/apppot/apppot-snap.base'
+base_state_file='/var/lib/apppot/apppot-snap.base'
 exclude_file='/var/lib/apppot/apppot-snap.exclude'
 
 
@@ -51,6 +52,11 @@ die () {
   (echo -n "$PROG: ERROR: ";
       if [ $# -gt 0 ]; then echo "$@"; else cat; fi) 1>&2
   exit $rc
+}
+
+warn () {
+  (echo -n "$PROG: WARNING: ";
+      if [ $# -gt 0 ]; then echo "$@"; else cat; fi) 1>&2
 }
 
 have_command () {
@@ -96,49 +102,63 @@ action=$1
 
 require_command tar
 require_command mktemp
+require_command mountpoint
 
-if ! [ -d $(dirname "$base_snap_file") ]; then
-    die 1 "Directory '$(dirname "$base_snap_file")' that should host snapshot files, is non-existent."
+if ! [ -d $(dirname "$base_state_file") ]; then
+    die 1 "Directory '$(dirname "$base_state_file")' that should host snapshot files, is non-existent."
 fi
 
 if ! [ -r "$exclude_file" ]; then
     die 1 "Exclude file '$exclude_file' cannot be read."
 fi
 
+# let the default archive location be what the init script expects
+if mountpoint -q "$HOME/job"; then
+    default_archive="$HOME/job/apppot-changes.tar.gz"
+else
+    default_archive="apppot-changes.tar.gz"
+    warn "Default archive location '$HOME/job' does not look like a hostfs mount, storing changes into '$default_archive' instead."
+fi
+
 case $action in
 
     base) 
+        if test -r "$base_state_file"; then
+            warn "Deleting old base state file '$base_state_file', interrupting this command may leave the system without a valid base state."
+            rm -f "$base_state_file"
+        fi
         tar $verbose --create -f /dev/null -C / . \
-            --listed-incremental="$base_snap_file" \
+            --listed-incremental="$base_state_file" \
             --exclude-from="$exclude_file" --exclude-backups \
             --anchored --wildcards-match-slash
         ;;
 
     changes) 
-        archive="${2:-apppot-changes.tgz}"
+        archive="${2:-$default_archive}"
         # check that the snapfile exists and can be read
-        test -r "$snapfile" \
-            || die 1 "Base snapshot '$snapfile' does not exist."
+        test -r "$base_state_file" \
+            || die 1 "Base state file '$base_state_file' does not exist."
         # create a temporary copy of the tar snapfile, as tar modifies
         # it during a `--create` operation
         snapfile=`mktemp` \
-            || die 1 "Cannot create temporary snapshot file."
+            || die 1 "Cannot create temporary state file."
         trap "{ rm -f '$snapfile'; }" EXIT
-        $maybe cp $verbose "$base_snap_file" "$snapfile" \
-            || die 1 "Cannot copy snapshot file to temporary location '$snapfile'."
+        $maybe cp $verbose "$base_state_file" "$snapfile" \
+            || die 1 "Cannot copy base state file to temporary location '$snapfile'."
         $maybe tar $verbose --create --auto-compress -f "$archive" -C / . \
             --listed-incremental="$snapfile" \
             --one-file-system \
             --exclude-from="$exclude_file" --exclude-backups \
             --exclude="$snapfile" --exclude="$archive" --exclude="$exclude_file" \
             --anchored --wildcards-match-slash
+        echo "Saved changes from base state into file '$archive'."
         ;;
 
     merge) 
-        archive="${2:-apppot-changes.tgz}"
+        archive="${2:-$default_archive}"
         $maybe tar $verbose --extract --auto-compress -f "$archive" -C / \
             --same-owner --same-permissions \
-            --listed-incremental="$base_snap_file" \
+            --listed-incremental="$base_state_file" \
             --exclude-from="$exclude_file" --exclude-backups \
             --exclude="$snapfile" --exclude="$archive" --exclude="$exclude_file" \
             --anchored --wildcards-match-slash
